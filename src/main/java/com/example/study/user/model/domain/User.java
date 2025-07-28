75c1c1388899fe53b0b44773d3244ed17d0e03c3
변영우 <byw1666@wjcompass.com>
2025-07-28 16:58:15 +0900

    3주차 과제 - JPA CRUD

---
+++ src/main/java/com/example/study/user/model/domain/User.java
@@ -0,0 +1,32 @@
+package com.example.study.user.model.domain;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * @author : 변영우 byw1666@wjcompass.com
+ * @since : 2025-07-28
+ */
+
+@Getter
+@Entity
+@Table(name = "USERS")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+    @Id
+    @Column(name = "USER_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "NAME")
+    private String name;
+}
